e12688137bb0a574d4ba36d88f1b48091bfa5fc9
Dandan Meng <mengdandanno1@163.com>
2021-08-09 23:44:22 +0800

    @Singleton

--- app/src/main/java/com/ddmeng/howhiltworks/AnalyticsService.kt
+++ app/src/main/java/com/ddmeng/howhiltworks/AnalyticsService.kt
@@ -2,7 +2,9 @@ package com.ddmeng.howhiltworks
 
 import android.util.Log
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class AnalyticsService @Inject constructor() {
 
     init {
