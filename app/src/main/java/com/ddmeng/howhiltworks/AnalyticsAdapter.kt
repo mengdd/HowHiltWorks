6f5257cb928e6b955031d0ce6b254508cea2c385
Dandan Meng <mengdandanno1@163.com>
2021-08-09 23:53:52 +0800

    make another @Singleton

--- app/src/main/java/com/ddmeng/howhiltworks/AnalyticsAdapter.kt
+++ app/src/main/java/com/ddmeng/howhiltworks/AnalyticsAdapter.kt
@@ -1,7 +1,9 @@
 package com.ddmeng.howhiltworks
 
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class AnalyticsAdapter @Inject constructor(
     private val service: AnalyticsService
 )
