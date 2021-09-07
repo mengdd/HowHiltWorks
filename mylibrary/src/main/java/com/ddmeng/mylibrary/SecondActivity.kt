f8bcea6fe013a850468a60b16f3d0ca52dddb283
Dandan Meng <mengdandanno1@163.com>
2021-09-07 23:28:00 +0800

    sample for @AssistedInject

--- mylibrary/src/main/java/com/ddmeng/mylibrary/SecondActivity.kt
+++ mylibrary/src/main/java/com/ddmeng/mylibrary/SecondActivity.kt
@@ -1,19 +1,43 @@
 package com.ddmeng.mylibrary
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.EntryPointAccessors
+import dagger.hilt.android.components.ActivityComponent
 
 @AndroidEntryPoint
 class SecondActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel: SecondViewModel = viewModel()
+            val viewModel: SecondViewModel = noteDetailViewModel(noteId = "note-id")
             Text(text = "Second")
         }
     }
+}
+
+@EntryPoint
+@InstallIn(ActivityComponent::class)
+interface ViewModelFactoryProvider {
+    fun noteDetailViewModelFactory(): SecondViewModel.Factory
+}
+
+@Composable
+fun noteDetailViewModel(noteId: String): SecondViewModel {
+    val factory = EntryPointAccessors.fromActivity(
+        LocalContext.current as Activity,
+        ViewModelFactoryProvider::class.java
+    ).noteDetailViewModelFactory()
+
+    return viewModel(factory = SecondViewModel.provideFactory(factory, noteId))
 }
--- mylibrary/src/main/java/com/ddmeng/mylibrary/SecondViewModel.kt
+++ mylibrary/src/main/java/com/ddmeng/mylibrary/SecondViewModel.kt
@@ -1,21 +1,39 @@
 package com.ddmeng.mylibrary
 
 import android.util.Log
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
+import androidx.lifecycle.ViewModelProvider
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
 
-@HiltViewModel
-class SecondViewModel @Inject constructor(
-    private val savedStateHandle: SavedStateHandle,
-    private val buildInformation:  BuildInformation
+class SecondViewModel @AssistedInject constructor(
+    private val buildInformation: BuildInformation,
+    @Assisted private val noteId: String
 ) : ViewModel() {
     init {
         Log.i("SecondViewModel", "build information: $buildInformation")
+        Log.i("SecondViewModel", "note id: $noteId")
     }
 
     fun print() {
         Log.i("SecondViewModel", "print")
+    }
+
+    @AssistedFactory
+    interface Factory {
+        fun create(noteId: String): SecondViewModel
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    companion object {
+        fun provideFactory(
+            assistedFactory: Factory,
+            noteId: String
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return assistedFactory.create(noteId) as T
+            }
+        }
     }
 }
