99f9425de532b812ad5f1f355e2f1c4c1e3cee69
Dandan Meng <mengdandanno1@163.com>
2023-04-25 09:09:08 +0800

    another experiment with const val; rename images

--- app/src/main/java/com/ddmeng/howhiltworks/utils/MyDebugTool.kt
+++ app/src/main/java/com/ddmeng/howhiltworks/utils/MyDebugTool.kt
@@ -22,22 +22,23 @@ class NoOpLogTool : LogTool {
     }
 }
 
+const val constBooleanFlag = true
 @Module
 @InstallIn(SingletonComponent::class)
 object MyLogToolModule {
 
     @Provides
     fun provideLogTool(): LogTool {
-        return if (BuildConfig.testFlag) {
+        return if (constBooleanFlag) {
             MyDebugLogTool()
         } else {
             NoOpLogTool()
         }
     }
     // in release build, viewed by jadx-gui, NoOpLogTool is returned directly when using BuildConfig.DEBUG
-    // this is done even when `minifyEnabled false` for release
-    // See ./images/release-build-LogTool.png
+    // this is done even when `minifyEnabled false` for release, debug build did the same way
 
-    // but if we change to use another customised flag, the release build still checks the flag
-    // See ./images/release-build-LogTool-using-test-flag.png
+    // but if we change to use another customised flag, the build still checks the flag
+    // tried const val flag, same behavior as BuildConfig.DEBUG
+
 }
